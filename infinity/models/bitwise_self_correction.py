d6348dfcb4d0197b5434af69fed45e318a02dad4
Felix-Zhenghao <20307100005@fudan.edu.cn>
2025-02-24 09:10:26 +0800

    finetune vae infra bsc

--- infinity/models/bitwise_self_correction.py
+++ infinity/models/bitwise_self_correction.py
@@ -1,24 +1,11 @@
 import os
+import cv2
 import os.path as osp
 
 import torch
 import torch.nn.functional as F
 import numpy as np
 
-
-def labels2image(all_indices, label_type='int_label', scale_schedule=None):
-    summed_codes, recons_imgs = self.vae.decode_from_indices(all_indices, scale_schedule, label_type)
-    recons_img = recons_imgs[0]
-    recons_img = (recons_img + 1) / 2
-    recons_img = recons_img.permute(1, 2, 0).mul_(255).cpu().numpy().astype(np.uint8)[:,:,::-1]
-    return recons_img
-
-def features2image(raw_features):
-    recons_imgs = self.vae.decode(raw_features.squeeze(-3))
-    recons_img = recons_imgs[0]
-    recons_img = (recons_img + 1) / 2
-    recons_img = recons_img.permute(1, 2, 0).mul_(255).cpu().numpy().astype(np.uint8)[:,:,::-1]
-    return recons_img
 
 class BitwiseSelfCorrection(object):
     def __init__(self, vae, args):
@@ -29,7 +16,7 @@ class BitwiseSelfCorrection(object):
         self.vae = vae
         self.debug_bsc = args.debug_bsc
 
-    def flip_requant(self, vae_scale_schedule, inp_B3HW, raw_features, device):
+    def flip_requant(self, vae_scale_schedule, inp_B3HW, raw_features, device, save_path = None, should_save=True):
         with torch.amp.autocast('cuda', enabled = False):
             B = raw_features.shape[0]
             if raw_features.dim() == 4:
@@ -40,12 +27,14 @@ class BitwiseSelfCorrection(object):
             gt_all_bit_indices = []
             pred_all_bit_indices = []
             x_BLC_wo_prefix = []
+            all_losses = []
             for si, (pt, ph, pw) in enumerate(vae_scale_schedule):
                 residual = codes_out - cum_var_input
                 if si != len(vae_scale_schedule)-1:
                     residual = F.interpolate(residual, size=vae_scale_schedule[si], mode=self.vae.quantizer.z_interplote_down).contiguous()
                 quantized, _, bit_indices, loss = self.vae.quantizer.lfq(residual) # quantized shape: [B, d_vae, 1, h, w], bit_indices shape: [B,1,h,w,d_vae]
                 gt_all_bit_indices.append(bit_indices)
+                all_losses.append(loss)
                 if si < self.noise_apply_layers:
                     noise_apply_strength = np.random.randint(0, 100 * self.noise_apply_strength+1) * 0.01
                     mask = torch.rand(*bit_indices.shape).to(device) < noise_apply_strength
@@ -79,19 +68,39 @@ class BitwiseSelfCorrection(object):
             x_BLC_wo_prefix = torch.cat(x_BLC_wo_prefix, 1)
 
             if self.debug_bsc:
-                self.visualize(vae_scale_schedule, inp_B3HW, gt_all_bit_indices, pred_all_bit_indices)
+                recon_loss = self.visualize(vae_scale_schedule, inp_B3HW, gt_all_bit_indices, pred_all_bit_indices, save_path=save_path, should_save=should_save)
+            
+            # get losses
+            all_losses = torch.mean(torch.stack(all_losses, dim = -1))
         
-        return x_BLC_wo_prefix, gt_ms_idx_Bl
+        return x_BLC_wo_prefix, gt_ms_idx_Bl, all_losses, recon_loss
     
-    def visualize(self, vae_scale_schedule, inp_B3HW, gt_all_bit_indices, pred_all_bit_indices):
-        gt_img = (inp_B3HW.squeeze(-3) + 1) / 2 * 255
-        gt_img = gt_img[0].permute(1,2,0).cpu().numpy().astype(np.uint8)[:,:,::-1]
-        recons_img_2 = labels2image(gt_all_bit_indices, label_type='bit_label', scale_schedule=vae_scale_schedule)
-        recons_img_3 = labels2image(pred_all_bit_indices, label_type='bit_label', scale_schedule=vae_scale_schedule)
-        cat_image = np.concatenate([gt_img, recons_img_2, recons_img_3], axis=1)
-        save_path = osp.abspath('non_teacher_force.jpg')
-        cv2.imwrite(save_path, cat_image)
-        print(f'Save to {save_path}')
-        import pdb; pdb.set_trace()
-        print(cat_image.shape)
+    def visualize(self, vae_scale_schedule, inp_B3HW, gt_all_bit_indices, pred_all_bit_indices, save_path = None, should_save=True):
+        # gt_img = (inp_B3HW.squeeze(-3) + 1) / 2 * 255
+        # gt_img = gt_img[0].permute(1,2,0).cpu().numpy().astype(np.uint8)[:,:,::-1]
+        recons_img_2, recon_loss = self.labels2image(inp_B3HW, gt_all_bit_indices, label_type='bit_label', scale_schedule=vae_scale_schedule) # TODO: check which loss to use
+        recons_img_3, _ = self.labels2image(inp_B3HW, pred_all_bit_indices, label_type='bit_label', scale_schedule=vae_scale_schedule) # TODO: check which loss to use
+        # cat_image = np.concatenate([gt_img, recons_img_2, recons_img_3], axis=1)
+        
+        # if should_save:
+        #     save_path = osp.abspath('non_teacher_force.jpg') if save_path is None else save_path
+        #     cv2.imwrite(save_path, cat_image)
+        #     print(f'Save to {save_path}')
+        
+        return recon_loss
+        
+    def labels2image(self, inp_B3HW, all_indices, label_type='int_label', scale_schedule=None):
+        summed_codes, recons_imgs = self.vae.decode_from_indices(all_indices, scale_schedule, label_type)
+        recons_loss = torch.nn.functional.mse_loss(recons_imgs, inp_B3HW)
+        # recons_img = recons_imgs[0]
+        # recons_img = (recons_img + 1) / 2
+        # recons_img = recons_img.detach().permute(1, 2, 0).mul_(255).cpu().numpy().astype(np.uint8)[:,:,::-1]
+        return 0, recons_loss
+
+    def features2image(self, raw_features):
+        recons_imgs = self.vae.decode(raw_features.squeeze(-3))
+        recons_img = recons_imgs[0]
+        recons_img = (recons_img + 1) / 2
+        recons_img = recons_img.permute(1, 2, 0).mul_(255).cpu().numpy().astype(np.uint8)[:,:,::-1]
+        return recons_img
         
